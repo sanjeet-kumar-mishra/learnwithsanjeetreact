202389ea866a1825278afe54b49484c0af19a601
Sanjeet Kumar Mishra <sanjitkumarmishra4@gmail.com>
2023-03-08 20:35:46 +0530

    employeedetails

--- src/components/Services/EmployeeDetails.js
+++ src/components/Services/EmployeeDetails.js
@@ -4,24 +4,28 @@ import "./EmployeeDetails.css"
 
 const EmployeeDetails = () => {
 
-    const [data, setData] = useState('')
-    const [employee, setEmployee] = useState('')
+    const [data, setData] = useState("")
+    const [employee, setEmployee] = useState("")
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        const response = await fetch('/services/employeedetails', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json', },
-            body: JSON.stringify({ data })
+        const response = await fetch("/services/employeedetails", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
         })
         const result = await response.json()
         console.log(result)
         setEmployee(result)
     }
 
-    const handleChange = (e) => {
-        setData(e.target.value)
+    const onChangeHandler = (e) => {
+        setData({
+            ...data, [e.target.name]: e.target.value
+        })
     }
 
     return (
@@ -30,10 +34,10 @@ const EmployeeDetails = () => {
                 <div className="row">
                     <form onSubmit={handleSubmit}>
                         <div className="col-sm-12 col-md-3 col-4-lg py-2">
-                            <label className="col-form-label text-white"><strong>Check Employee Status</strong></label>
+                            <label className="col-form-label text-white"><strong>Check Employee Details</strong></label>
                         </div>
                         <div className="col-sm-12 col-md-3 col-4-lg py-2">
-                            <input className="form-control" id="searchemployee" name="searchemployee" value={data.searchemployee} placeholder="Search Here" onChange={handleChange} required
+                            <input className="form-control" id="searchemployee" name="searchemployee" value={data.searchemployee} placeholder="Search Here" onChange={onChangeHandler} required
                                 type="text" />
                         </div>
                         <div className="col-sm-12 col-md-3 col-4-lg py-2">
@@ -43,9 +47,11 @@ const EmployeeDetails = () => {
                 </div>
 
                 <div className="row">
-
-                    <h5 className="py-1">Name:<span> {employee.NAME}</span></h5>
-
+                    <div className="col-auto">
+                            {Object.entries(employee).map(([key, value], i) => (
+                                    <h6 key={i}>{key} : {value}</h6>
+                                ))}
+                    </div>
                 </div>
             </div>
         </>
